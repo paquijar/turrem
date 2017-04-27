47dc356065ee1fc9f5044eb775be2614143193af
MauricioHoyosArdila <mhoyosa2@eafit.edu.co>
2017-04-27 00:43:52 -0500

    cambio de contraseña listo

--- servidorRapido.js
+++ servidorRapido.js
@@ -3,29 +3,51 @@ const express = require('express')
 const http = require('http')
 const os = require('os')
 const bodyParser = require('body-parser')
-
+const bcrypt = require('bcryptjs')
 const app = express()
 const influx = new Influx.InfluxDB('http://localhost:8086/hola')
+const salt = bcrypt.genSaltSync(10);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 function usuario(user_name,password,response) {
-    influx.query('select * from "user"').then(results => {
+    influx.query('select * from usuario').then(results => {
         var ayud=JSON.stringify(results[results.length-1]);
     ayud = ayud.substring(ayud.indexOf("contraseña")+13,ayud.length);
     var contraseña = ayud.substring(0,ayud.indexOf("\""));
     ayud = ayud.substring(ayud.indexOf("correo")+9,ayud.length);
     var correo = ayud.substring(0,ayud.indexOf("\""));
-    //console.log(user_name+ " "+correo+" "+password+" "+contraseña);
-    if (user_name === correo && password === contraseña) {
+    if (user_name === correo && bcrypt.compareSync(password, contraseña) ) {
         response.end("yes");
     }
     else {response.end("no")}
+});
+}
 
+function cambiar(pass_actual, pass_nueva, response) {
+
+    influx.query('select * from usuario').then(results => {
+        var ayud=JSON.stringify(results[results.length-1]);
+    ayud = ayud.substring(ayud.indexOf("contraseña")+13,ayud.length);
+    var contraseña = ayud.substring(0,ayud.indexOf("\""));
+    if ( bcrypt.compareSync(pass_actual, contraseña) ) {
+        var hash = bcrypt.hashSync(pass_nueva, salt);
+        var correo1= 'mauriciohoyosardila@gmail.com';
+        //console.log(pass_actual+" "+pass_nueva);
+        influx.dropSeries({ measurement: m => m.name('usuario') });
+        setTimeout( function (){influx.writePoints([
+            {
+                measurement: 'usuario',
+                fields: {correo: correo1, contraseña: hash, value: 0},
+            }
+        ]);
+            response.end("yes");}, 100);
+    }
+    else {response.end("no")}
 });
 
 }
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get('/',function(request, response){
   response.sendFile(__dirname+'/interfaz_grafica/login.html');
@@ -54,6 +76,13 @@ app.post('/login',function(request,response){
     usuario(user_name,password,response);
 });
 
+app.post('/cambiar',function(request,response){
+
+    var passActual=request.body.passActual;
+    var passNueva=request.body.passNueva;
+    cambiar(passActual, passNueva, response);
+
+});
 
 
 app.listen(8083, function(){
